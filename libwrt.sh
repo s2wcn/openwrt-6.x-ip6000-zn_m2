0decbeb2b2ebd86507e155ee490860ac832ba164
s2wcn <54429435+s2wcn@users.noreply.github.com>
2025-10-16 13:57:49 +0800

    Update libwrt.sh to modify OpenVPN installation
    Removed installation command for OpenVPN configuration.

--- libwrt.sh
+++ libwrt.sh
@@ -1,3 +1,6 @@
 rm -rf package/emortal/luci-app-athena-led
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
+
+# 删除或注释掉安装 etc/config/openvpn 的语句
+sed -i '/etc\/config\/openvpn/d' feeds/luci/applications/luci-app-openvpn-server/Makefile
