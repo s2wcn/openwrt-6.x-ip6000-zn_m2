58e221d4c8f31d2d0a512d79d7f98a021110641b
s2wcn <54429435+s2wcn@users.noreply.github.com>
2026-04-30 13:24:25 +0800

    Update preset-adguard-core.sh

--- scripts/preset-adguard-core.sh
+++ scripts/preset-adguard-core.sh
@@ -2,7 +2,13 @@
 
 mkdir -p files/usr/bin/AdGuardHome
 
-AGH_CORE=$(curl -sL https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
+# 增加 GitHub API 鉴权头
+AUTH_HEADER=""
+if [ -n "$GITHUB_TOKEN" ]; then
+  AUTH_HEADER="-H \"Authorization: Bearer $GITHUB_TOKEN\""
+fi
+
+AGH_CORE=$(eval curl -sL $AUTH_HEADER https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest | grep /AdGuardHome_linux_${1} | awk -F '"' '{print $4}')
 
 wget -qO- $AGH_CORE | tar xOvz > files/usr/bin/AdGuardHome/AdGuardHome
 
